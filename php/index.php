285bef03f747b738cc2cddf247fa8d9793320d0b
Valhom <vad2381@yandex.ru>
2021-02-04 01:03:57 -0500

    feb-4-2021 - bugs fixed

--- php/index.php
+++ php/index.php
@@ -4,8 +4,8 @@
 		$email = $_POST['email'];
 		$message = $_POST['message'];
 		$human = intval($_POST['human']);
-		$from = 'Demo Contact Form'; 
-		$to = 'example@domain.com'; 
+		$from = 'Contact Form';
+		$to = 'sales@idigitalstudio.ca';
 		$subject = 'Message from Contact Demo ';
 		
 		$body ="From: $name\n E-Mail: $email\n Message:\n $message";
